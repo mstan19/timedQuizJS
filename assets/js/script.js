351e22df0599f8f734189dd37858c3699c5dae53
melissastan <melissastan91@gmail.com>
2022-07-31 16:14:48 -0400

    question layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 const startBtn = document.getElementById('startBtn');
-
-
 startBtn.addEventListener("click", function(event) {
 
-})
+
+function startBtn {
+
+}
+
+function goNextQuestion {
+
+}
+
+function pickAnswer {
+
+}
+
+
+
+
+    
