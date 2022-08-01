0cff136dbb08c21124e219598d3a13a3b52a6a37
melissastan <melissastan91@gmail.com>
2022-08-01 01:10:27 -0400

    added a var for ListOfQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const startBtn = document.getElementById('startBtn');
 const questionContainerElement = document.getElementById("questionContainer"); 
+var listOfQuestions = document.getElementById('questionContainer');
 
 
 startBtn.addEventListener("click", startQuiz) 
@@ -8,9 +9,33 @@ function startQuiz () {
     console.log("start")
     startBtn.classList.add("hideThis");
     questionContainerElement.classList.remove("hideThis");
-    GoToNextQuestion ();
+}
+//Goal: Get html element and assign it to  what you want to change
+//get element html = array of question[object in that array].attributes in that object
+function StartQuiz () {
+    document.getElementById('question') = ListofQuestion[currentQuestion].question
+    document.getElementById('answerA') = ListofQuestion[currentQuestion].answerA
+    document.getElementById('answerB') = ListofQuestion[currentQuestion].answerB
+    document.getElementById('answerC') = ListofQuestion[currentQuestion].answerC
+    document.getElementById('answerD') = ListofQuestion[currentQuestion].answerD
 }
 
-function GoToNextQuestion () {
-
-}
+var listOfQuestions = [
+    {
+        Question: "What is JS?",
+        answerA: "dog",
+        answerB: "cat",
+        answerC: "bird",
+        answerD: "js is",
+        correctAnswer: "answerA",
+    },
+    
+    {
+        Question:"What is html?",
+        answerA: "bear",
+        answerB: "lion",
+        answerC: "tiger",
+        answerD: "html is",
+        correctAnswer: "answerB"
+    }
+]
