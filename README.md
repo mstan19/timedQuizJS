e955932eff500663830cd40d8cf2b92dc733aa13
melissastan <melissastan91@gmail.com>
2022-08-01 23:07:12 -0400

    fixed the timer

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # timedQuizJS
+The purpose of this assignment is to create a quiz, where the user is rewarded 10 points for answering the question correctly  and penalizes -10 seconds from the clock ifor answering the question incorrectly.
+
+## Website Screenshot
+This is what the quiz looks like when the user loads the quiz to the browser.
+
+![Quiz mock-up](./images/quizms.png)
+
+## How Code Was Refractored
+* Adding and calling functions to help perform a certain task.
+* Creating speific variables to serve different purpose.
+* Creating a js file that makes the html more dynamic
+* Adding CSS to html to style it
+
+## How to deploy the link application
+* Click on this link for deployed application
+* https://mstan19.github.io/PasswordGenerator/
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,19 @@ var answerB = document.getElementById("answerB");
 var answerC = document.getElementById("answerC");
 var answerD = document.getElementById("answerD");
 var currentQuestion = 0;
-var congrats = document.getElementById("congrats");
-var theReveal = document.getElementById("theReveal");
-var enterInitials = document.getElementById("enterInitials");
 var initials = document.getElementById("initials");
-var sendHighscore = document.getElementById("sendHighscore");
 var result = document.getElementById("result");
+var highScore = document.getElementById("highScore");
+var sendHighScore = document.getElementById("sendHighScore");
 
-startBtn.addEventListener("click", startQuiz) ;
+startBtn.addEventListener("click", startQuiz);
+
 
 function startQuiz () {
-    console.log("start")
+    //console.log("start")
     startBtn.classList.add("hideThis");
     questionContainerElement.classList.remove("hideThis");
+    //console.log("goS");
     ruleInfo.classList.add("hideThis");
     title.classList.add("hideThis");
    
@@ -33,13 +33,14 @@ jsToHtml();
 //Goal: Get html element and assign it to  what you want to change
 //get element html = array of question[object in that array].attributes in that object
 function jsToHtml () {
-
+console.log("yo");
     document.getElementById("question").innerHTML = listOfQuestions[currentQuestion].question;
     document.getElementById("answerA").innerHTML = listOfQuestions[currentQuestion].answerA;
     document.getElementById("answerB").innerHTML = listOfQuestions[currentQuestion].answerB;
     document.getElementById("answerC").innerHTML = listOfQuestions[currentQuestion].answerC;
     document.getElementById("answerD").innerHTML = listOfQuestions[currentQuestion].answerD;
     
+setTime();
 }
 
 answerA.addEventListener("click", clickedAnswerA);
@@ -51,21 +52,48 @@ answerD.addEventListener("click", clickedAnswerD);
 //this includes the questionstem and answer choices
 var listOfQuestions = [
     {
-        question: "What is JS?",
-        answerA: "dog",
-        answerB: "cat",
-        answerC: "bird",
-        answerD: "js is",
+        question: "An HTML document can contain _____.",
+        answerA: "Attributes",
+        answerB: "Tags",
+        answerC: "Raw text",
+        answerD: "All the answers are true",
         correctAnswer: "answerD",
     },
     
     {
-        question:"What is html?",
-        answerA: "bear",
-        answerB: "lion",
-        answerC: "tiger",
-        answerD: "html is",
-        correctAnswer: "answerB"
+        question:"Choose the correct HTML tag for a large title.",
+        answerA: "H1",
+        answerB: "Heading",
+        answerC: "Head",
+        answerD: "H6",
+        correctAnswer: "answerA"
+    }, 
+
+    {
+        question: "We can make rounded borders around elements using which CSS element?",
+        answerA: "Border-collapse",
+        answerB: "Border-round",
+        answerC: "Border-radius",
+        answerD: "None of the above",
+        correctAnswer: "answerC",
+    },
+    
+    {
+        question:"Which of the following are valid CSS position property values?",
+        answerA: "Static",
+        answerB: "Relative",
+        answerC: "Fixed",
+        answerD: "All of the above",
+        correctAnswer: "answerD"
+    }, 
+
+    {
+        question: "Which of the following variables takes precedence over the others if the names are the same?",
+        answerA: "Global variable",
+        answerB: "The local element",
+        answerC: "Both",
+        answerD: "None of the Above",
+        correctAnswer: "answerB",
     }
 ]
 
@@ -90,17 +118,29 @@ function clickedAnswerD () {
     //console.log("start3")
 }
 //Timer section
-var secondsLeft = 60;
+  var secondsLeft = 30;
 function setTime() {
     var timeInterval = setInterval(function () {
+    //console.log(secondsLeft);
     document.getElementById('count').innerHTML = secondsLeft + " seconds remaining";
     secondsLeft--;
-    if (secondsLeft === 0) {
-        clearInterval(interval);
+        //console.log(secondsLeft);
+    if (secondsLeft === 0 || currentQuestion >= listOfQuestions.length) {
+        clearInterval(timeInterval);
         document.getElementById('count').innerHTML = 'No Time Left';
-        alert("You're out of time!");
+        alert("Quiz is Over!");
+        endQuiz();
     }
+    if (secondsLeft >= 10) {
+
+    }
+    else {
+        secondsLeft = 0;
+    endQuiz();
+    }
+    
     }, 1000);
+
 }
 
 //then ... the user's answer will be compared to the correct answer
@@ -134,6 +174,7 @@ function goToNextQuestion () {
 
 //score and store in Local storage
 function endQuiz () {
+
     document.getElementById("question").classList.add("hideThis");
     answerA.classList.add("hideThis");
     answerB.classList.add("hideThis");
@@ -146,11 +187,38 @@ function endQuiz () {
     initials.textContent;
 
     result.classList.remove("hideThis");
+    questionContainerElement.classList.add("hideThis");
+
    }
 
-document.getElementById("sendHighscore").onclick = function() {
+    sendHighScore.onclick = function() {
     initialsEl.value;
     var finalScore = currentScore;
-    localStorage.setItem (initialsEl.value, finalScore);
-   // console.log("ms");
+    localStorage.setItem ("Your initals", initialsEl.value);
+    localStorage.setItem ("Your Score", finalScore);
+    //console.log("ms");
+    //console.log("hat");
+    result.classList.add("hideThis");
+    highScore.classList.remove("hideThis");
+
+    pastHighScore();
+}
+
+highScore.addEventListener("click", pastHighScore);
+
+
+function pastHighScore() {
+    var finalScore = currentScore;
+    var getInitials = localStorage.getItem ("Your initals");
+    var getScore = localStorage.getItem ("Your Score");
+    console.log(finalScore);
+    
+    // for (var i = 0; i < getScore.length; i++) {
+    //     getScore[i].initialsEl;
+    //     getScore[i].finalScoreEl;
+    // }
+    var data = JSON.parse(localStorage.getItem(getScore));
+    data.textContent;
+   
+    
 }
